2a7efd26a06a1a8355583ee09574325a159347d0
Idalis Valadez <idalisvaladez22@gmail.com>
2023-10-09 22:23:02 -0500

    complete lab

--- server/models.py
+++ server/models.py
@@ -12,6 +12,22 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('name')
+    def validates_name(self, key, name):
+        names = db.session.query(Author.name).all()
+        if name == '':
+            raise ValueError("Name is required")
+        elif name in names:
+            raise ValueError("Name must be unique")
+        return name
+
+    @validates('phone_number')
+    def validate_phone_number(self, key, number):
+        if len(number) == 10:
+            return number
+        else:
+            raise ValueError("Number must be 10 digits")
+
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
@@ -27,6 +43,29 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('title')
+    def validate_title(self, key, title):
+        if "Won't Belive" not in title and "Secret" not in title and "Top" not in title and "Guess" not in title:
+            raise ValueError("Posts must have a clickbait-y title")
+        return title
+
+    @validates('content')
+    def validate_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError("Content must be at least 250 characters long")
+        return content
+
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if len(summary) >= 250:
+            raise ValueError("Summary must be less than 250 characters")
+        return summary
+    
+    @validates('category')
+    def validate_category(self, key, category):
+        if category != 'Fiction' and category != 'Non-Fiction':
+            raise ValueError("Category must be either Fiction or Non-Fiction")
+        return category
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
